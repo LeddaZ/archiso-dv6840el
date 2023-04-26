79b85a011ebae8b56e92d73aa4666039f2f54ee8
Leonardo Ledda <leonardoledda@gmail.com>
2023-04-26 17:16:29 +0200

    archiso-dv6840el: Customize ISO name
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="archlinux-dv6840el"
+iso_label="ARCH_6840_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Leonardo Ledda <leonardoledda@gmail.com>"
+iso_application="Arch Linux for HP Pavilion dv6840el"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
